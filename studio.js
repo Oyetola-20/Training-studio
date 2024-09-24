a4a046d37b97bd19207771d7397946a76f426488
Martinez <oyetolamateen14@gmail.com>
2024-09-23 19:22:38 -0700

    changes

---
+++ studio.js
@@ -0,0 +1,6 @@
+'use strict'
+
+let class1 = document.querySelector('.one');
+let class2 = document.querySelector('.two');
+let class3 = document.querySelector('.three');
+let class4 = document.querySelector('.four');
